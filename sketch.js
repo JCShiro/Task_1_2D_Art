f2227be6e7cefbd5ff330c4cd90c89a000351ad9
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2025-12-11 16:23:32 +0000

    Added square co-ordinaes
    Created an array that stores the co-ordinates of the squares for later implimentation of the click function.
    Use array to store circle co-ordinates.
    UPDATE DOCUMENTATION BEFORE NEXT CHANGES.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
-console.log("sketch.js is linked")
-console.log(test_variable)
+console.log("sketch.js is linked");
+console.log(test_variable);
 //=========GLOBAL VARIABLES=========\\
 let first = true;
+//Array of square co-ordinates
+let squares = [];
+//Array of circle co-ordinates
+let circles = [];
 
 //=========MAIN CODE=========\\
 //Setup function, only called once.
@@ -9,7 +13,10 @@ function setup() {
   //creates a canvas 700x700px
   const canvas = createCanvas(700, 700);
 
-  canvas.parent("canvas")
+  canvas.parent("canvas");
+
+  createSquares();
+  console.log(squares);
 }
 
 //Draw function. called endlessley (unless noLoop is used)
@@ -23,16 +30,17 @@ function draw() {
   //Sets background color of the canvas
   background(1);
   //calls the function that creates the grid
-  drawSquare();
+  // drawSquare();
+  //makes the squares
+  drawSquares(x, y);
 }
 
 //=========Functions=========\\
+
 /**
  * Creates the squares that make up the grid
  */
-function drawSquare() {
-  //sets the color of the squares
-  fill(primaryRed, primaryGreen, primaryBlue);
+function createSquares() {
   //loop to create squares on the x-axis
   for (x = 0; x < 10; x++) {
     //loop to create squares on the y-axis
@@ -40,11 +48,20 @@ function drawSquare() {
       // creates a square 70px wide at point 0,0 and is
       // looped to create a new square every 70px
       square(0 + 70 * x, 0 + 70 * y, 70, 10);
+      squares.push({ x: 70 * x, y: 70 * y });
     }
   }
   //console log to check the squares are loaded successfully. For testing
-  if (first) {
-    console.log("done loading squares");
-    first = false;
+  // if (first) {
+  console.log("done loading squares"); //Log for debugging
+  // first = false;
+}
+// }
+
+function drawSquares(x, y) {
+  for (let s of squares) {
+    //sets the color of the squares
+    fill(primaryRed, primaryGreen, primaryBlue);
+    square(s.x, s.y, 70, 10);
   }
 }
